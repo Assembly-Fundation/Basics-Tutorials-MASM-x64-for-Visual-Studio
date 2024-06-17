c1b044ea8c2d9a7d7ae8746a6bbdb2c49643e727
Reymon-Real <148560553+Reymon-Real@users.noreply.github.com>
2024-06-17 16:18:51 -0600

    Update HelloWorld.asm

--- Tutorial/Output/C-Assembly-Integration/HelloWorld.asm
+++ Tutorial/Output/C-Assembly-Integration/HelloWorld.asm
@@ -1,24 +1,42 @@
-; Importamos las bibliotecas estandar de Microsoft Visual C (MSVC)
-includelib ucrt.lib ; Universal C Runtime
-includelib legacy_stdio_definitions.lib ; Definición de las funciones de la biblioteca estandar de C stdio
+; We import the standard libraries from Microsoft Visual C (MSVC)
+includelib ucrt.lib ; Universal C Runtime, provides standard C runtime functions
+includelib legacy_stdio_definitions.lib ; Definitions for standard C stdio library functions
 
-; Importamos las bibliotecas del sistema operativo
-includelib kernel32.lib ; Biblioteca estandar para entrada y salida de datos
+; We import the operating system libraries
+includelib kernel32.lib ; Standard library for operating system interactions
 
-extern printf: proc ; Declaramos que printf es una función externa perteneciente a las bibliotecas de C importadas
-extern ExitProcess: proc ; Declaramos que ExitProcess es una función externa perteneciente a kernel32
-                         ; Sirve que una vez terminado el programa, podamos devolver el control al sistema operativo
+extern printf: proc ; We declare that printf is an external function from the imported C libraries
+extern ExitProcess: proc ; We declare that ExitProcess is an external function from kernel32
+                         ; It allows us to return control to the operating system once the program has finished
 
-.data ; Nuestra sección de datos
-  ; Declaramos un espacio de almacenamiento de la siguiente forma
-  ; name + stack + data or reserve
-  msg db "Hello, Wolrd!", 0
+.data ; Our data section
+  ; We declare a storage space in the following format:
+  ; name + stack + data or reserve for later use
+  msg db "Hello, World!", 0 ; The zero indicates that the message ends at the last character
 
-.code ; Nuestra sección de código
-  ; La función principal se llama main
-  main proc ; Inicio de la función
-    sub rsp, 32
-    add rsp, 32
-    xor
-  main endp ; Fin de la función
-end ; fin del archivo
+.code ; Our code section
+  ; The main function is called main
+  main proc ; Start of the function
+    ; In x64 assembly, 'proc' starts a procedure definition and 'endp' ends it.
+
+    sub rsp, 32 ; We reserve 32 bytes on the stack for local variables or function calls.
+                ; This space can be used for saving registers or local data.
+
+      mov rcx, offset msg ; We pass rcx as the first argument to printf.
+                          ; In x64 calling convention, the first four arguments are passed via registers RCX, RDX, R8, and R9.
+                          ; The 'offset' keyword is used to get the memory address of 'msg'.
+                          ; The data stored in 'msg' is moved to the rcx register.
+
+      call printf         ; We call printf to print our message.
+                          ; 'call' is used to invoke a procedure or function.
+
+    add rsp, 32 ; We release the 32 bytes reserved on the stack.
+                ; It's important to clean up and maintain stack balance.
+
+    xor rcx, rcx ; We indicate that we are exiting the program with a return code of 0.
+                 ; 'xor' with itself is a common way to set a register to zero.
+
+    call ExitProcess ; We exit the program, returning control to the operating system.
+                     ; This ensures a clean exit without leaving any processes hanging.
+  main endp ; End of the function
+end ; End of file
